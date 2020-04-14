1db5a7a21ad29d440f7845189324543a111d4491
dmatthew <dmatthew@cars.com>
2020-04-14 14:35:53 -0600

    determing a winner

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Square from '../Square/Square'
 
 class Board extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       squares: Array(9).fill(null),
@@ -23,12 +23,37 @@ class Board extends React.Component {
     return <Square
       value={this.state.squares[i]}
       onClick={() => this.handleClick(i)}
-      />;
+    />;
+  }
+
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
   render() {
-    console.log('State**', this.state)
-    const status = 'Next player: X';
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
